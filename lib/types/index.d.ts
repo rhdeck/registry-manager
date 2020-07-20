7b28f475e075d388e879c77311bd4f8286c231d1
Ray Deck <ray@raydeck.com>
2020-07-20 12:44:04 -0500

    Remove and ignore lib

--- lib/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare const registry: {
-    [key: string]: any;
-};
-declare const get: (key: string, defaultValue?: any) => any;
-declare const set: (key: string, value: string) => string;
-export default registry;
-export { get, set };

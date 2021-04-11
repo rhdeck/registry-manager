e6b9a55a55069031a96f8267a83175ab0ed1d4fb
Ray Deck <ray@raydeck.com>
2021-04-11 07:36:44 -0500

    feat: add exists function to help identifiy whether a value is present without triggering warnings

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 const registry: { [key: string]: any } = {};
-const get = (key: string, defaultValue?: any) => {
+export const get = (key: string, defaultValue?: any) => {
   if (!registry[key] && !process.env[key]) {
     console.warn(`${key} not in registry or env var`);
   }
   return registry[key] || process.env[key] || defaultValue;
 };
-const set = (key: string, value: string) => (registry[key] = value);
+export const exists = (key: string) => {
+  return registry[key] || process.env[key];
+};
+export const set = (key: string, value: string) => (registry[key] = value);
 export default registry;
-export { get, set };
